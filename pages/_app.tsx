2a1018a8b7837b2934299892d20ddea9d3136b61
kzkpro <kishor.khambu@gmail.com>
2025-08-22 23:27:20 +0545

    table updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app'
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,36 @@ import axios from 'axios'
 import { Container, Table } from 'react-bootstrap'
 
 interface Ipo {
-  // define IPO fields according to API response
-  IPOName: string
-  IssueDate: string
-  [key: string]: string
+  ipo_id: number
+  company_name: string
+  stock_symbol: string
+  share_registrar?: string
+  sector_name?: string
+  file_name?: string
+  share_type?: string
+  price_per_unit?: string
+  rating?: string
+  units?: number
+  min_units?: number
+  max_units?: number
+  local_units?: number
+  general_units?: number
+  promoter_units?: number
+  mutual_fund_units?: number
+  other_units?: number
+  total_amount?: number
+  opening_date_ad?: string // or Date if you parse it
+  opening_date_bs?: string
+  closing_date_ad?: string // or Date
+  closing_date_bs?: string
+  closing_time?: string
+  extended_date_ad?: string // or Date
+  extended_date_bs?: string
+  extended_time?: string
+  status?: string
+  fiscal_year_ad?: string
+  fiscal_year_bs?: string
+  culture_code?: string
 }
 
 export default function Home() {
@@ -15,22 +41,58 @@ export default function Home() {
 
   useEffect(() => {
     async function loadIpos() {
-      const { data } = await axios.get('/api/ipos')
-      setIpos(data.items || [])
+      try {
+        const { data } = await axios.get('/api/ipos')
+        setIpos(data || [])
+      } catch (err) {
+        console.error('Error fetching IPOs:', err)
+      }
     }
     loadIpos()
   }, [])
 
   return (
-    <Container className='p-3'>
-      <h1>IPO Alerts</h1>
-      <Table>
-        {ipos.map((ipo, idx) => (
-          <tr key={idx}>
-            <td>{ipo.IPOName}</td>
-            <td>{ipo.IssueDate}</td>
+    <Container className='p-2'>
+      <h1 className='text-center my-4'>IPO Data</h1>
+      <Table striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>Company Name</th>
+            <th>Stock Symbol</th>
+            <th>Share Type</th>
+            <th>Price Per Unit</th>
+            <th>Units</th>
+            <th>Min Units</th>
+            <th>Max Units</th>
+            <th>Status</th>
+            <th>Opening Date (AD)</th>
+            <th>Opening Date (BS)</th>
+            <th>Closing Date (AD)</th>
+            <th>Closing Date (BS)</th>
+            <th>Fiscal Year (AD)</th>
+            <th>Fiscal Year (BS)</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {ipos.map((ipo, idx) => (
+            <tr key={idx}>
+              <td>{ipo.company_name}</td>
+              <td>{ipo.stock_symbol}</td>
+              <td>{ipo.share_type}</td>
+              <td>{ipo.price_per_unit}</td>
+              <td>{ipo.units}</td>
+              <td>{ipo.min_units}</td>
+              <td>{ipo.max_units}</td>
+              <td>{ipo.status}</td>
+              <td>{ipo.opening_date_ad}</td>
+              <td>{ipo.opening_date_bs}</td>
+              <td>{ipo.closing_date_ad}</td>
+              <td>{ipo.closing_date_bs}</td>
+              <td>{ipo.fiscal_year_ad}</td>
+              <td>{ipo.fiscal_year_bs}</td>
+            </tr>
+          ))}
+        </tbody>
       </Table>
     </Container>
   )
